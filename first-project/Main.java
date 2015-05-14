e2d84d84a707eca2d718713572122701f27d110b
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2015-05-14 05:25:13 -0700

    bring over first Hello World app to java-trails directory

---
+++ first-project/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        
+        String text = "Hello";
+        
+        switch (text) {
+        case "Hello":
+            System.out.println("Yo");
+
+        break;
+        
+        default:
+            System.out.println("Boo!");
+        }
+
+        System.out.println("Hello World!");
+
+        int number = 3;
+
+        System.out.println(number);
+    }
+}
