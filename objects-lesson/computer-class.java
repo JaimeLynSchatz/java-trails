df98f368577e4b44aa2f4c8e5a2879cbadbc772f
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2015-05-14 21:31:09 -0700

    beginnings of computer class -- ha ha

---
+++ objects-lesson/computer-class.java
@@ -0,0 +1,18 @@
+public Class Computer() {
+  private String name;
+
+  private int memory;
+
+  private String brand;
+
+  public void setName(name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+
+
+
